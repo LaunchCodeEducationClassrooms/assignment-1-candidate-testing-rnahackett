a4c6df5d69e4da145db3237318c6d85428c8e8f9
rnahackett <rnahackett@gmail.com>
2021-02-21 21:49:20 +0000

    Made multiple questions and added grade

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,9 +8,9 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions 
-let correctAnswers 
-let candidateAnswers
+let questions = ['Who was the first American woman in space? ','True or false: 5 kilometer == 5000 meters? ','(5 + 3)/2 * 10 = ? ',"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",'What is the minimum crew size for the ISS? '];
+let correctAnswers = ['Sally Ride','true','40','Trajectory','3'];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -20,16 +20,31 @@ candidateName = input.question("Enter your name: ")
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question ("Who was the first American woman in space? ")
-
+  for (let i = 0; i < questions.length; i++) {
+    candidateAnswers[i] =input.question(questions[i]);
+  }
+  return candidateAnswers;
 }
-
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-console.log("Correct Answer: Sally Field")
+  let numCorrect = 0;
+  console.log(`\nCandidate Name: ${candidateName}`);
+    for (let i = 0; i < questions.length; i++) {
+      console.log(`\n${i + 1}) ${questions[i]}\nYour Answer: ${candidateAnswers[i]}\nCorrect Answer: ${correctAnswers[i]}\n`);
+      if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
+        numCorrect++
+    }
+  }
 
-  let grade;
+  let grade = (numCorrect / questions.length) * 100;
+  
+  console.log(`>>> Overall Grade: ${grade}% (${numCorrect} of ${questions.length} correct) <<<`);
+  if (grade >= 80) {
+    console.log(`>>> Status: PASSED <<<`);
+  } else {
+    console.log(`>>> Status: FAILED <<<`);
+  }
   
 
   return grade;
